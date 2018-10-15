068e123e44549974b82e3e59236eb0e5d871e20a
Jan Hadaš <jan.hadas@i.cz>
2018-10-15 16:12:54 +0200

    JEP 213 - try with resources on final vars, diamond op on anonymous classes

--- src/test/java/cz/i/test/JavaTest.java
+++ src/test/java/cz/i/test/JavaTest.java
@@ -2,7 +2,11 @@ package cz.i.test;
 
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.List;
 
 import static java.util.Arrays.asList;
 
@@ -17,6 +21,31 @@ public class JavaTest {
 
         System.out.println("cislo: " + number);
         System.out.println("polozka z kolekce: " + item.orElse("Kolekce je prazdna"));
+    }
+
+    @Test
+    public void tryWithResourcesTest() throws FileNotFoundException  {
+        // effectively final var
+        PrintStream out = new PrintStream(new FileOutputStream("test.out"));
+
+        // some code ...
+
+        // try with resources closes the output stream
+        try (out) {
+            out.println(158);
+        }
+    }
+
+    @Test
+    public void anonymouseClassDiamondTest() {
+
+        List<String> fakeList = new ArrayList<>(){
+            @Override
+            public int size() {
+                // always empty
+                return 0;
+            }
+        };
 
     }
 }
